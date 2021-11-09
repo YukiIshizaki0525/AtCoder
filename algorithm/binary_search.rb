85799c67ef246bc9c1ca1c9c3847db69afdc4613
Yuki Ishizaki <ishiyuki405@icloud.com>
2021-11-09 20:34:51 +0900

    abc225_cとITP1_1_b完了

---
+++ algorithm/binary_search.rb
@@ -0,0 +1,34 @@
+array = [ 11, 13, 17, 19, 23, 29, 31 ]
+
+require 'byebug'
+byebug
+def binary_search(array, target)
+
+  head = 0
+  tail = array.count - 1
+
+  while head <= tail
+
+    center = (head + tail) / 2
+
+    if array[center] == target
+      return "index = #{center}"
+    elsif array[center] < target
+      head = center + 1
+      # 右半分の範囲に絞る
+    else
+      tail = center - 1
+      # 左半分の範囲に絞る
+    end
+
+  end
+
+  return "index is nothing"
+
+end
+
+# 探したい値
+target = 17
+
+# 二分探索法（バイナリサーチ）の実行
+p binary_search(array, target)
