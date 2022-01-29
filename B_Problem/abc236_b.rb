c22feae8cab99c4df3d05d7d9c87d34cb68ea9c4
Yuki Ishizaki <ishiyuki405@icloud.com>
2022-01-29 16:53:23 +0900

    B問題に解説追加

--- B_Problem/abc236_b.rb
+++ B_Problem/abc236_b.rb
@@ -1,16 +1,32 @@
 # B - Who is missing?
 # 自分の回答
-n = gets.to_i
-a = gets.split.map(&:to_i)
-res = []
-ano_hash = {}
-a.each do |num|
-  ano_hash[num].nil? ? ano_hash[num] = 1 : ano_hash[num] += 1
-end
-ano_hash.each do |k, v|
-  res.push(k) if v == 3
-end
+# n = gets.to_i
+# a = gets.split.map(&:to_i)
+# res = []
+# ano_hash = {}
+# a.each do |num|
+#   ano_hash[num].nil? ? ano_hash[num] = 1 : ano_hash[num] += 1
+# end
+# ano_hash.each do |k, v|
+#   res.push(k) if v == 3
+# end
 
-puts res
+# puts res
+
+# 模範回答
+N = gets.chomp.to_i
+arr = gets.chomp.split(' ').map(&:to_i)
+arr = arr.sort
+# arr = [1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3]
+
+require 'byebug'
+byebug
+
+for i in 1..N do
+  if arr[(i*4)-2] != arr[(i*4)-1] #=> 数字iが書かれたカードの3枚目と4枚目の比較、一致しないなら欠けている4枚目がないため、それが求めている答えになる
+    puts i
+    break
+  end
+end
 
 
