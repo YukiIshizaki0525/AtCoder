2c35d22aa0362bcde14bd613a1087f2a59f5df22
Yuki Ishizaki <ishiyuki405@icloud.com>
2022-03-20 20:58:15 +0900

    abc243完了

--- B_Problem/abc243_b.rb
+++ B_Problem/abc243_b.rb
@@ -1,10 +1,21 @@
 # B - Hit and Blow
 # 自分の回答 AC
 
-gets.to_i
-a = gets.split.map &:to_i
-b = gets.split.map &:to_i
+# gets.to_i
+# a = gets.split.map &:to_i
+# b = gets.split.map &:to_i
 
-no_1 = [a,b].transpose.count{|a,b| a == b}
-puts no_1
-puts [a,b].flatten.tally.count{|k,v| v==2} - no_1
+# no_1 = [a,b].transpose.count{|a,b| a == b}
+# puts no_1
+# puts [a,b].flatten.tally.count{|k,v| v==2} - no_1
+
+# 模範回答
+N = gets.to_i
+A = gets.split.map(&:to_i)
+B = gets.split.map(&:to_i)
+
+n =  (0...N).count{|i| A[i] == B[i]} #別の配列のi番目の値と同じものがいくつあるかカウント
+puts n
+puts A.product(B).count{|a,b| a == b} - n
+# A = [1,3,5,2], B = [2,3,1,4]
+# => [[1, 2], [1, 3], [1, 1], [1, 4], [3, 2], [3, 3], [3, 1], [3, 4], [5, 2], [5, 3], [5, 1], [5, 4], [2, 2], [2, 3], [2, 1], [2, 4]]
--- C_Problem/abc243_c.rb
+++ C_Problem/abc243_c.rb
@@ -1,14 +1,46 @@
 # C - Collision 2
-n = gets.to_i
-ary = n.times.map{ gets.split.map &:to_i }
-s = gets.chomp
-res = "No"
+# n = gets.to_i
+# ary = n.times.map{ gets.split.map &:to_i }
+# s = gets.chomp
+# res = "No"
 
-ary.each.with_index{ |a, i| a.unshift(s[i]) }
-ary = ary.group_by(&:last).delete_if{|a,b| b.count==1}.values
+# ary.each.with_index{ |a, i| a.unshift(s[i]) }
+# ary = ary.group_by(&:last).delete_if{|a,b| b.count==1}.values
 
-ary.each do |a|
-  a.each do |ar|
-    p ar
+# ary.each do |a|
+#   a.each do |ar|
+#     p ar
+#   end
+# end
+
+# 模範回答
+N = gets.to_i #=> 3
+Z = N.times.map{gets.split.map(&:to_i)} #=> [[2, 3], [1, 1], [4, 1]]
+S = gets.chomp.chars #=> ["R", "R", "L"]
+
+require 'byebug'
+byebug
+
+right = Hash.new(10**10)
+left = Hash.new(-1)
+
+(0...N).each do |i|
+  x = Z[i][0] #=>x=2, x=1, x=4
+  y = Z[i][1] #=>y=3, y=1, y=1
+
+  if S[i] == 'R'
+    right[y] = [right[y], x].min
+    #=>right[3](10000000000)とx(2)の比較 => right[3]=2
+    #=>right[1](10000000000)とx(1)の比較 => right[1]=1
+    #=>right[1](10000000000)とx(1)の比較 => right[1]=4
+  else
+    left[y] = [left[y],x].max #=> left[y](-1)とx(4)の比較=>left[y]=4
+  end
+
+  if right[y] < left[y]
+    puts 'Yes'
+    exit
   end
 end
+
+puts 'No'
